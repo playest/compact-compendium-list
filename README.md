e74c26699ca049e50054f50d6b92d032732061f1
playest <playest.ff@laposte.net>
2022-01-17 21:16:59 +0100

    re-render the tab when a change has been made
    everything seems to be working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Better UI for the compendium packs tab. Clearer and more compact.
+
+This module could almost be without JS and be CSS only but, sadly, it's needed because the
--- better-compendium-list.ts
+++ better-compendium-list.ts
@@ -1,5 +1,45 @@
 declare const Hooks: any;
 
+function getDocumentClassFromTitle(title: string) {
+    if(title === "Actor") {
+        return "compendium-of-actor";
+    }
+    else if(title === "Cards") {
+        return "compendium-of-card";
+    }
+    else if(title === "Item") {
+        return "compendium-of-item";
+    }
+    else if(title === "JournalEntry") {
+        return "compendium-of-journalentry";
+    }
+    else if(title === "Macro") {
+        return "compendium-of-macro";
+    }
+    else if(title === "Playlist") {
+        return "compendium-of-playlist";
+    }
+    else if(title === "RollTable") {
+        return "compendium-of-rolltable";
+    }
+    else if(title === "Scene") {
+        return "compendium-of-scene";
+    }
+}
+
+function renderCompendiumTree() {
+    document.querySelectorAll("#compendium .compendium-type h3").forEach(h3 => {
+        const documentClass = getDocumentClassFromTitle(h3.innerHTML);
+        if(documentClass == undefined) {
+            console.error("Unindentified document class in compendium list:", h3.innerHTML);
+            return;
+        }
+        h3.parentElement?.querySelector(".compendium-list")?.classList.add(documentClass);
+    });
+}
+
 Hooks.once('init', () => {
-    console.log("Better Compendium List loaded");
+    Hooks.on('renderCompendiumDirectory', () => {
+        renderCompendiumTree();
+    });
 });
