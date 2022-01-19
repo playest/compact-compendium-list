bd29825e82ac66d4a13e699b60c85982a0a13a98
playest <playest.ff@laposte.net>
2022-01-19 21:56:50 +0100

    update scripting and gha

--- scripts/packzip.sh
+++ scripts/packzip.sh
@@ -3,6 +3,8 @@
 no_check_opt=""
 no_check_env=0
 
+# TODO make a loop to read the arguments, this code needs a specific order for the arguments which doesn't really makes sense
+
 if [ "$1" = "--nocheck" ]
 then
     no_check_opt="--nocheck"
@@ -37,6 +39,16 @@ then
     fi
 fi
 
+if [ "$1" = "--set-shortname" ]
+then
+    echo "shortname set to" $2
+    shortname="$2"
+    shift
+    shift
+else
+    shortname=v"$version"
+fi
+
 NO_CHECK=$no_check_env npm pack
 
 pack_archive_name="$npm_package_name-$npm_package_version.tgz"
@@ -44,20 +56,22 @@ pack_archive_name="$npm_package_name-$npm_package_version.tgz"
 if [ -f "$pack_archive_name" ]
 then
     echo "Pack archive ($pack_archive_name) detected."
-    mkdir -p "releases/v$version/"
+    mkdir -p "releases/$shortname/"
+    tar xzf "$pack_archive_name" -C "releases/$shortname/"
     # Create updated module.json (with version number taken from package.json)
-    ./scripts/replace_version.sh $version "module.json" > "releases/v$version/module.json"
-    tar xzf "$pack_archive_name" -C "releases/v$version/"
-    cd "releases/v$version/"
+    ./scripts/replace_version.sh "$version" "$shortname" "module.json" > "releases/$shortname/module.json"
+    cd "releases/$shortname/"
     # Rename the directory
-    mv "package" "$npm_package_name-$version"
+    mv "package" "$npm_package_name-$shortname"
     # Copy updated module.json
-    cp module.json "$npm_package_name-$version"
-    zip -r "$npm_package_name-$version.zip" "$npm_package_name-$version"
+    cp module.json "$npm_package_name-$shortname"
+    git log --pretty=format:'%H' -n 1 > "$npm_package_name-$shortname/hash.txt"
+    zip -r "$npm_package_name-$shortname.zip" "$npm_package_name-$shortname"
     cd ../..
-    rm -r "releases/v$version/$npm_package_name-$version"
+    rm -r "releases/$shortname/$npm_package_name-$shortname"
 else
     echo "Pack archive ($pack_archive_name) not found. Will not do anything."
+    exit 1
 fi
 
 rm "$pack_archive_name"
--- scripts/replace_version.sh
+++ scripts/replace_version.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 version="$1"
-module_json_path="$2"
+shortname="$2"
+module_json_path="$3"
 
-jq --arg version "$version" '(.version, .download, .manifest) |= gsub("{{version}}"; $version)' "$module_json_path"
+jq --arg version "$version" --arg shortname "$shortname" '(.version, .download, .manifest) |= gsub("{{version}}" ; $version) | (.version, .download, .manifest) |= gsub("{{shortname}}"; $shortname)' "$module_json_path"
